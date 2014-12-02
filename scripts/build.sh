e6d9e746ed68ab570c8c2583f7d6cd20cc54fb5b
Matthew Flood <mflood@shutterstock.com>
2014-12-01 21:49:00 -0700

    spelling, build script enhancements

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+mkdir -p target/classes
+mkdir -p target/libs
 javac  -d target/classes src/main/java/com/eteam/studentregistration/*.java
 jar cfm target/libs/eteam.jar manifest.txt -C target/classes com/eteam/studentregistration/
 
